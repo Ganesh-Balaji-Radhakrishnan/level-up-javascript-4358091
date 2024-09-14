ab9ae05fff8197478f25eee2aa191645ae6989cc
Ganesh Balaji Radhakrishnan <ganesh.krishnan237@gmail.com>
2024-09-14 17:30:26 +0000

    created a prototype fun

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,24 @@
-// Write your code here
+function Book(title,author,ISBN,numCopies){
+  this.title = title;
+  this.author = author;
+  this.ISBN = ISBN;
+  this.numCopies = numCopies
+}
+
+Book.prototype.getAvailablity = function(){
+  return this.numCopies
+}
+
+Book.prototype.restock = function(num = 1){
+  this.numCopies += num
+}
+
+Book.prototype.sell = function(num = 5){
+  this.numCopies -= num
+}
+
+const HarryPotter = new Book('Harry Potter', 'JKR', 12332, 4)
+
+console.log(HarryPotter.getAvailablity())
+HarryPotter.restock(100)
+console.log(HarryPotter.getAvailablity())
