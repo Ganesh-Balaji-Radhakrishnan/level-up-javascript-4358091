4e95dd7e15fb87be3f663313b75bc9bb7288b52b
Ganesh Balaji Radhakrishnan <ganesh.krishnan237@gmail.com>
2024-09-14 20:32:10 +0000

    futher tasks

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,16 @@
 // Write your code here
+
+function Movie(title, dir, genre, relYear, rating){
+  this.title = title
+  this.dir = dir
+  this.genre = genre
+  this.relYear = relYear
+  this.rating = rating
+}
+
+Movie.prototype.getInfo = function(){
+  return `${this.title}, a ${this.genre} film directed by ${this.dir} was released in ${this.relYear}. it received a rating of ${this.rating}`
+}
+
+const HarryPotter = new Movie('Harry Potter', 'JKR', 12332, 2000, 5)
+console.log(HarryPotter.getInfo())
--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,31 @@
 // Write your code here
+
+/* class User{
+  #username
+  #password
+  constructor(username, password, age){
+    this.#username = username
+    this.#password = password
+    this.age = age
+  }
+
+  printUsername(){
+    console.log(`the name is ${this.#username}`)
+  }
+}
+
+const Ganesh = new User("Ganesh", "Balaji", 30)
+Ganesh.printUsername() */
+
+//alternate method to use a private identfier
+
+const username = Symbol("username")
+const password = Symbol("password")
+
+const user = {
+  [username] : "Ganesh",
+  [password] : "Balaji",
+  age: 30
+}
+
+console.log(user.username)
